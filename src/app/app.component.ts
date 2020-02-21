7f33fba2f1261a94adda02c589c16fd6f09bfd0b
HSP <40883035+Hspharwinder@users.noreply.github.com>
2020-02-21 15:12:03 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'MultiSelect';
+  items = [
+    { id: 1, name: 'Python' },
+    { id: 2, name: 'Node Js' },
+    { id: 3, name: 'Java' },
+    { id: 4, name: 'PHP', disabled: true },
+    { id: 5, name: 'Django' },
+    { id: 6, name: 'Angular' },
+    { id: 7, name: 'Vue' },
+    { id: 8, name: 'ReactJs' },
+  ];
+  selected = [];
+
+  multiSelecteValue(){
+    console.log("selected ", this.selected);
+  }
+}
